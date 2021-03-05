2229aec44717796d851b5abde09cfc9da89bfecd
Anais1210 <azhang2@myges.fr>
2021-03-05 20:25:03 +0100

    update

--- java/SixQuiPrend.java
+++ java/SixQuiPrend.java
@@ -5,9 +5,20 @@ public class SixQuiPrend {
     private static final int NB_CARD_MAX = 104;
 
     public static int findNumberOfBullHeads(int value) {
-        // TODO
+        int headBull=0;
 
-        return 0;
+       if(value == 55){
+           headBull = 7;
+       } else if (value % 11 == 0){
+           headBull = 5;
+       } else if (value % 10 == 0){
+           headBull = 3;
+       } else if (value % 5 == 0){
+           headBull = 2;
+       } else {
+           headBull = 1;
+       }
+        return headBull;
     }
 
     public static void gameLoop() {
